349d317d128da2044bdcec24883cfd57e66e5add
Tyuiu.SpirinAA@outlook.com <Tyuiu.SpirinAA@outlook.com>
2023-11-09 19:24:20 +0500

    Task7

---
+++ Tyuiu.SpirinAA.Sprint4.Task7.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SpirinAA.Sprint4.Task7.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SpirinAA.Sprint4.Task7.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea3b77eb-8a2d-479a-8593-1da2f1a587e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SpirinAA.Sprint4.Task7.V11/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SpirinAA.Sprint4.Task7.V11.Lib;
+
+namespace Tyuiu.SpirinAA.Sprint4.Task7.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int m = 2;
+            int[,] mtrx = new int[n, m];
+
+            string str = "56789012";
+
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Спирин А. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Спирин Александр Алексеевич | АСОиУб-23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 56789012. Преобразуйте ее в матрицу   *");
+            Console.WriteLine("* 4 на 2 и подсчитайте количество нечетных чисел в матрице.               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(n, m, str);
+
+            Console.WriteLine("Количество нечетных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
